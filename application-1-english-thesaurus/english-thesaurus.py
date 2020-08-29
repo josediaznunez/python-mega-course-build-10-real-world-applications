380b82077661dbe9df07e77ac50c1583c84bdc0e
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-08-29 18:56:06 -0500

    move file reading outside method; extract variables for word to search; get user input for word to search

--- application-1-english-thesaurus/english-thesaurus.py
+++ application-1-english-thesaurus/english-thesaurus.py
@@ -1,9 +1,14 @@
 
 import json
 
+with open('./application-1-english-thesaurus/data.json') as definitions_file:
+  definitions = json.load(definitions_file)
+
 def get_definition(word):
-  with open('./application-1-english-thesaurus/data.json') as definitions_file:
-    definitions = json.load(definitions_file)
   return definitions[word]
 
-print(get_definition("rain"))
+word = input("Enter word: ")
+
+definition = get_definition(word)
+
+print(definition)
