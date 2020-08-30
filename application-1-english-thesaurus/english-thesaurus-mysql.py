e62040cc47ea02d2e00af2c1e390e1ebaf866efd
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-08-30 15:15:30 -0500

    remove commented code

--- application-1-english-thesaurus/english-thesaurus-mysql.py
+++ application-1-english-thesaurus/english-thesaurus-mysql.py
@@ -1,25 +1,3 @@
-# import mysql.connector
-
-# connection = mysql.connector.connect(
-#     user = "ardit700_student",
-#     password = "ardit700_student",
-#     host = "108.167.140.122",
-#     database = "ardit700_pm1database"
-# )
-
-# cursor = connection.cursor()
-
-# word = input("Enter a word: ")
-
-# query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
-# results = cursor.fetchall()
-
-# if results:
-#     for result in results:
-#         print(result[1])
-# else:
-#     print("No word found!")
-####################################################################################
 import mysql.connector
 import difflib
 
