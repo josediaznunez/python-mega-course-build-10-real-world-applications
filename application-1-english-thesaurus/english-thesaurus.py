f25a44e510e7ca8abf4b8e889974619a978f7ef6
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-08-29 19:00:04 -0500

    test commit

--- application-1-english-thesaurus/english-thesaurus.py
+++ application-1-english-thesaurus/english-thesaurus.py
@@ -2,7 +2,7 @@
 import json
 
 with open('./application-1-english-thesaurus/data.json') as definitions_file:
-  definitions = json.load(definitions_file)
+  definitions = json.load(definitions_file) 
 
 def get_definition(word):
   return definitions[word]
