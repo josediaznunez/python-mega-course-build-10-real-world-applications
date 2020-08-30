b8b9775a1f056904ad4904789805e5d7812dda9b
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-08-29 19:12:47 -0500

    format word to search for

--- application-1-english-thesaurus/english-thesaurus.py
+++ application-1-english-thesaurus/english-thesaurus.py
@@ -5,8 +5,9 @@ with open('./application-1-english-thesaurus/data.json') as definitions_file:
   definitions = json.load(definitions_file) 
 
 def get_definition(word):
-  if word in definitions:
-    return definitions[word]
+  word_formatted = word.strip().lower()
+  if word_formatted in definitions:
+    return definitions[word_formatted]
   else:
     return "The word doesn't exist. Please double check it."
 
