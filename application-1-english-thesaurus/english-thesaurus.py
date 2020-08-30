b518d6539379f2ad6979bba47b2e09695da4823f
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-08-29 19:09:12 -0500

    add conditional in method for non-existing words

--- application-1-english-thesaurus/english-thesaurus.py
+++ application-1-english-thesaurus/english-thesaurus.py
@@ -5,7 +5,10 @@ with open('./application-1-english-thesaurus/data.json') as definitions_file:
   definitions = json.load(definitions_file) 
 
 def get_definition(word):
-  return definitions[word]
+  if word in definitions:
+    return definitions[word]
+  else:
+    return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 
