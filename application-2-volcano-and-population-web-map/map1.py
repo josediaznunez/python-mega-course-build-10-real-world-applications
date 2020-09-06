4146d81d24e0985159f656646fecf7da947bfe81
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-09-06 15:14:01 -0500

    stylize popup text with html

--- application-2-volcano-and-population-web-map/map1.py
+++ application-2-volcano-and-population-web-map/map1.py
@@ -4,18 +4,20 @@ import pandas
 PROJECT_PATH = "/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/"
 DATA_PATH = "/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/Volcanoes.txt"
 
-map = folium.Map(location=[45, -121], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
-
 volcano_data = pandas.read_csv(DATA_PATH)
 lat = list(volcano_data["LAT"])
 lon = list(volcano_data["LON"])
 elev = list(volcano_data["ELEV"])
 
+map = folium.Map(location=[45, -121], zoom_start=6, tiles="Stamen Terrain")
+fg = folium.FeatureGroup(name="My Map")
+html = """<h4>Volcano information:</h4>Height: %s m"""
+
 for lat, lon, elev in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(elev), width=200, height=100)
     fg.add_child(folium.Marker(
             location=[lat, lon], 
-            popup=str(elev) + " m", 
+            popup=folium.Popup(iframe),
             icon=folium.Icon(color="green")
         )
     )
