9e04e3881d0c6d45d9ff5c77b3e062337fb48a9d
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-09-06 15:10:18 -0500

    add elevation to marker popups

--- application-2-volcano-and-population-web-map/map1.py
+++ application-2-volcano-and-population-web-map/map1.py
@@ -4,16 +4,18 @@ import pandas
 PROJECT_PATH = "/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/"
 DATA_PATH = "/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/Volcanoes.txt"
 
-map = folium.Map(location=[41.8, -88.1], zoom_start=6, tiles="Stamen Terrain")
+map = folium.Map(location=[45, -121], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 volcano_data = pandas.read_csv(DATA_PATH)
 lat = list(volcano_data["LAT"])
 lon = list(volcano_data["LON"])
+elev = list(volcano_data["ELEV"])
 
-for lat, lon in zip(lat, lon):
+for lat, lon, elev in zip(lat, lon, elev):
     fg.add_child(folium.Marker(
-            location=[lat, lon],  
+            location=[lat, lon], 
+            popup=str(elev) + " m", 
             icon=folium.Icon(color="green")
         )
     )
