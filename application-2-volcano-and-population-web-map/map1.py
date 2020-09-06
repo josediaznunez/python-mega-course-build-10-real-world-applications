1e0407d55b4cb777f0b2a5faa67550fb7ede9d44
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-09-05 19:29:25 -0500

    add multiple points

--- application-2-volcano-and-population-web-map/map1.py
+++ application-2-volcano-and-population-web-map/map1.py
@@ -1,9 +1,12 @@
 import folium
 
+PROJECT_PATH = "/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/"
+
 map = folium.Map(location=[41.8, -88.1], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[41.5, -88.5], popup="Downers Grove", icon=folium.Icon(color="green")))
+for coordinates in [[42.5, -87.5], [43.5, -86.5]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Downers Grove", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
-map.save("/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/Map1.html")
+map.save(PROJECT_PATH + "Map1.html")
