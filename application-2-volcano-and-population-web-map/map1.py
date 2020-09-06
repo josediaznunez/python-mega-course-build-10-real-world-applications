e0e6c8ca55beb754af999db19e464ea9c4ae002f
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2020-09-06 15:02:02 -0500

    add multiple points using volcano data CSV using longitude and latitude columns

--- application-2-volcano-and-population-web-map/map1.py
+++ application-2-volcano-and-population-web-map/map1.py
@@ -1,12 +1,22 @@
 import folium
+import pandas
 
 PROJECT_PATH = "/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/"
+DATA_PATH = "/home/jose/Development/python-mega-course-build-10-real-world-applications/application-2-volcano-and-population-web-map/Volcanoes.txt"
 
 map = folium.Map(location=[41.8, -88.1], zoom_start=6, tiles="Stamen Terrain")
-
 fg = folium.FeatureGroup(name="My Map")
-for coordinates in [[42.5, -87.5], [43.5, -86.5]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Downers Grove", icon=folium.Icon(color="green")))
+
+volcano_data = pandas.read_csv(DATA_PATH)
+lat = list(volcano_data["LAT"])
+lon = list(volcano_data["LON"])
+
+for lat, lon in zip(lat, lon):
+    fg.add_child(folium.Marker(
+            location=[lat, lon],  
+            icon=folium.Icon(color="green")
+        )
+    )
 
 map.add_child(fg)
 map.save(PROJECT_PATH + "Map1.html")
