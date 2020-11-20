b48939be664cdaa98a9c0e8dfbef8762e20a0e81
Diaz-Nunez-Jose <josediaznunez3@gmail.com>
2020-11-20 13:00:52 -0600

    display text to the user

--- application-5-feel-good-mobile-app/main.py
+++ application-5-feel-good-mobile-app/main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+import json, glob, random
 from datetime import datetime
+from pathlib import Path
 
 Builder.load_file('design.kv')
 
@@ -46,6 +47,18 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = 'right'
         self.manager.current = 'login_screen'
+    
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob('quotes/*txt')
+        available_feelings = [Path(filename).stem for filename in 
+                                available_feelings]
+        if feel in available_feelings:
+            with open(f'quotes/{feel}.txt') as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = 'Try another feeling'
 
 class MainApp(App):
     def build(self):
