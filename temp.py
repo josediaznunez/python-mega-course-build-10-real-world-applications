17ba1ecc5b7815db390f0b9e2dbe39ae0af250e4
Diaz-Nunez-Jose <josediaznunez3@gmail.com>
2020-11-20 17:08:27 -0600

    loading, displaying, resizing, and writing images

--- temp.py
+++ temp.py
@@ -1,43 +1,10 @@
-import psycopg2
+import cv2
 
-def create_table():
-  conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres' host='localhost' port='5432'")
-  cur = conn.cursor()
-  cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-  conn.commit()
-  conn.close()
- 
-def insert(item, quantity, price):
-  conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres' host='localhost' port='5432'")
-  cur = conn.cursor()
-  # cur.execute("INSERT INTO store VALUES ('%s', '%s', '%s')" % (item, quantity, price))
-  cur.execute("INSERT INTO store VALUES (%s, %s, %s)", (item, quantity, price))
-  conn.commit()
-  conn.close()
+img = cv2.imread('galaxy.jpg', 0)
 
-def view():
-  conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres' host='localhost' port='5432'")
-  cur = conn.cursor()
-  cur.execute("SELECT * FROM store")
-  rows = cur.fetchall()
-  conn.close()
-  return rows
 
-def delete(item):
-  conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres' host='localhost' port='5432'")
-  cur = conn.cursor()
-  cur.execute("DELETE FROM store WHERE item = %s", (item,))
-  conn.commit()
-  conn.close()
-
-def update(quantity, price, item):
-  conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres' host='localhost' port='5432'")
-  cur = conn.cursor()
-  cur.execute("UPDATE store SET quantity = %s, price = %s WHERE item = %s", (quantity, price, item))
-  conn.commit()
-  conn.close()
-
-update(3, 15.99 / 2 * 3, "Foam Finger")
-
-for row in view():
-  print(row)
+resized_image = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+cv2.imshow('Galaxy', resized_image)
+cv2.imwrite('Galaxy_resized.jpg', resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
